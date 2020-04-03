f48072264695c1396190a5ba6183880514393b76
kanumilliveena <veena.kanumilli@cloudtp.com>
2020-04-03 11:26:22 -0500

    Update slack-test.sh

--- slack-test.sh
+++ slack-test.sh
@@ -1,1 +1,1 @@
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Allow me to reintroduce myself!"}' https://hooks.slack.com/services/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
+
